f911b6b95737e704ff93443c659a17d30de12f04
DaveyDark <devesh97531@gmail.com>
2022-12-10 13:21:00 +0530

    Completed Day 1

---
+++ 2022/c/day1puz1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    FILE *input;
+    char line[20];
+    int cal,sum = 0,greatest = 0;
+    input = fopen("prog01.1.in","r");
+
+    while(fgets(line,20,input) != NULL ){
+        if(line[0]=='\n'){
+            if(sum>greatest)greatest = sum;
+            sum=0;
+            continue;
+        }
+        cal = atoi(line);
+        sum+=cal;
+        /* printf( "%s",line ); */
+    }
+
+    printf("%d is greatest\n",greatest);
+
+    fclose(input);
+}
+
---
+++ 2022/c/day1puz2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const int TOP_COUNT=5;
+
+void shift(int arr[TOP_COUNT],int ind,int val){
+    int ctr = TOP_COUNT-1;
+    while(ctr>=ind){
+        if(ctr == ind)arr[ctr] = val;
+        else arr[ctr] = arr[ctr-1];
+        ctr--;
+    }
+}
+
+int main(){
+    FILE *input;
+    char line[20];
+    int cal,sum = 0,top[TOP_COUNT];
+    input = fopen("prog01.1.in","r");
+
+    while(fgets(line,20,input) != NULL ){
+        if(line[0]=='\n'){
+            for (int i=0; i<TOP_COUNT; i++) {
+                if(sum>top[i]){
+                    shift(top, i, sum);
+                    sum=0;
+                    break;
+                }
+            }
+            sum=0;
+            continue;
+        }
+        cal = atoi(line);
+        sum+=cal;
+        /* printf( "%s",line ); */
+    }
+
+    for(int i=0;i<TOP_COUNT;i++){
+        printf("%d is #%d\n",top[i],i+1);
+    }
+
+    fclose(input);
+}
+
