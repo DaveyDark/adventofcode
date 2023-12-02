1158c3ff5956a55a204eda07ce67915e09f1da48
DaveyDark <devesh97531@gmail.com>
2023-12-02 15:19:23 +0530

    2023 Day 2 done

---
+++ 2023/rust/day02/src/part1.rs
@@ -0,0 +1,20 @@
+pub fn solve(input: String) -> u64 {
+    let mut sum = 0;
+    let mut id = 1;
+    for line in input.lines() {
+        let mut word_iter = line.split_whitespace().skip(2);
+        let mut possible = true;
+        while let Some(next) = word_iter.next() {
+            let count = next.trim_end_matches(':').parse::<u64>().unwrap();
+            match word_iter.next().unwrap().trim_end_matches(&[',',';']) {
+                "green" => possible &= count <= 13,
+                "red" => possible &= count <= 12,
+                "blue" => possible &= count <= 14,
+                _ => panic!(),
+            }
+        }
+        if possible {sum += id}
+        id += 1;
+    }
+    sum
+}
