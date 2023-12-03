46876725dabe8436b562d70d7de5c21304dffed4
DaveyDark <devesh97531@gmail.com>
2023-12-03 15:07:37 +0530

    2023 Day 3 done

---
+++ 2023/rust/day03/src/part2.rs
@@ -0,0 +1,43 @@
+use std::collections::HashSet;
+
+fn collect_number(grid: &Vec<Vec<char>>, i: i32, j: i32, visited: &mut HashSet<(i32,i32)>) -> u32 {
+    let mut left = j;
+    let mut right = j;
+    while right < grid[0].len() as i32 && grid[i as usize][right as usize].is_ascii_digit() { 
+        right += 1 
+    }
+    while left >= 0 && grid[i as usize][left as usize].is_ascii_digit() { 
+        left -= 1 
+    }
+    let mut num = 0;
+    for j in left+1..right {
+        if visited.insert((i,j)) {
+            num = num*10 + grid[i as usize][j as usize].to_digit(10).unwrap();
+        } else {
+            return 0;
+        }
+    }
+    num
+}
+
+pub fn solve(input: String) -> u32 {
+    let grid = input.lines().map(|l| l.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>();
+    let mut sum = 0;
+    let adj = [[-1,-1],[0,-1],[1,-1],[-1,0],[1,0],[-1,1],[0,1],[1,1]];
+    for i in 0..grid.len() {
+        for j in 0..grid[0].len() {
+            if grid[i][j] == '*' {
+                let mut visited = HashSet::new();
+                let mut numbers = Vec::new();
+                for [x,y] in &adj {
+                    let res = collect_number(&grid, i as i32 + *x, j as i32 + *y, &mut visited);
+                    if res != 0 { numbers.push(res) }
+                }
+                if numbers.len() == 2 {
+                    sum += numbers[0] * numbers[1];
+                }
+            }
+        }
+    }
+    sum
+}
