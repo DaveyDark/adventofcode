aa2c0f44cbb705841ee63bcb5525b3ea8da2f993
DaveyDark <devesh97531@gmail.com>
2023-12-04 21:05:33 +0530

    2023 Day 4 done

---
+++ 2023/rust/day04/src/part1.rs
@@ -0,0 +1,23 @@
+use std::collections::HashSet;
+
+pub fn solve(input: String) -> u32 {
+    let mut sum = 0;
+    for line in input.lines().map(|l| l.split(':').last().unwrap()) {
+        let mut line_iter = line.split('|');
+        let mut keys = HashSet::new();
+        let mut res = 0;
+        for key in line_iter.next().unwrap().split_whitespace() {
+            keys.insert(key.parse::<u32>().unwrap());
+        }
+        for val in line_iter.next().unwrap().split_whitespace() {
+            let val = val.parse::<u32>().unwrap();
+            if keys.contains(&val) {
+                res += 1;
+            }
+        }
+        if res > 0 {
+            sum += 2u32.pow(res-1);
+        }
+    }
+    sum
+}
