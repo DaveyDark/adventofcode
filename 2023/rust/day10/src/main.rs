b4654056fc01c00c272239c82672572f7ed410d7
DaveyDark <devesh97531@gmail.com>
2023-12-14 23:55:47 +0530

    2023 Day 10 Part 2 Done

--- 2023/rust/day10/src/main.rs
+++ 2023/rust/day10/src/main.rs
@@ -16,12 +16,16 @@ fn main() {
         Ok(data) => println!("Part 1: Input result: {}", part1::solve(data)),
         Err(e) => println!("Error reading actual input for part 1: {}", e),
     }
-    // match fs::read_to_string("input/sample.txt") {
-    //     Ok(data) => println!("Part 2: sample result: {}", part2::solve(data)),
-    //     Err(e) => println!("Error reading sample input for part 2: {}", e),
-    // }
-    // match fs::read_to_string("input/input.txt") {
-    //     Ok(data) => println!("Part 2: input result: {}", part2::solve(data)),
-    //     Err(e) => println!("Error reading actual input for part 2: {}", e),
-    // }
+    match fs::read_to_string("input/sample3.txt") {
+        Ok(data) => println!("Part 2: sample 1 result: {}", part2::solve(data)),
+        Err(e) => println!("Error reading sample input for part 2: {}", e),
+    }
+    match fs::read_to_string("input/sample4.txt") {
+        Ok(data) => println!("Part 2: sample 2 result: {}", part2::solve(data)),
+        Err(e) => println!("Error reading sample input for part 2: {}", e),
+    }
+    match fs::read_to_string("input/input.txt") {
+        Ok(data) => println!("Part 2: input result: {}", part2::solve(data)),
+        Err(e) => println!("Error reading actual input for part 2: {}", e),
+    }
 }
