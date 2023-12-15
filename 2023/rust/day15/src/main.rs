cc5304534ef080bc2f2976ae717be6e11c9712fe
DaveyDark <devesh97531@gmail.com>
2023-12-15 20:15:40 +0530

    2023 Day 15 Part 1 Done

---
+++ 2023/rust/day15/src/main.rs
@@ -0,0 +1,23 @@
+use std::fs;
+
+mod part1;
+// mod part2;
+
+fn main() {
+    match fs::read_to_string("input/sample.txt") {
+        Ok(data) => println!("Part 1: Sample 1 result: {}", part1::solve(data)),
+        Err(e) => println!("Error reading actual input for part 1: {}", e),
+    }
+    match fs::read_to_string("input/input.txt") {
+        Ok(data) => println!("Part 1: Input result: {}", part1::solve(data)),
+        Err(e) => println!("Error reading actual input for part 1: {}", e),
+    }
+    // match fs::read_to_string("input/sample.txt") {
+    //     Ok(data) => println!("Part 2: sample result: {}", part2::solve(data)),
+    //     Err(e) => println!("Error reading sample input for part 2: {}", e),
+    // }
+    // match fs::read_to_string("input/input.txt") {
+    //     Ok(data) => println!("Part 2: input result: {}", part2::solve(data)),
+    //     Err(e) => println!("Error reading actual input for part 2: {}", e),
+    // }
+}
