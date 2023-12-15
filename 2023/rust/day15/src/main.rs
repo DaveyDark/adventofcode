96620108cb6818eeb32ce0462741340151bbc4ce
DaveyDark <devesh97531@gmail.com>
2023-12-15 22:23:02 +0530

    2023 Day 15 Part 2 Done

--- 2023/rust/day15/src/main.rs
+++ 2023/rust/day15/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 mod part1;
-// mod part2;
+mod part2;
 
 fn main() {
     match fs::read_to_string("input/sample.txt") {
@@ -12,12 +12,12 @@ fn main() {
         Ok(data) => println!("Part 1: Input result: {}", part1::solve(data)),
         Err(e) => println!("Error reading actual input for part 1: {}", e),
     }
-    // match fs::read_to_string("input/sample.txt") {
-    //     Ok(data) => println!("Part 2: sample result: {}", part2::solve(data)),
-    //     Err(e) => println!("Error reading sample input for part 2: {}", e),
-    // }
-    // match fs::read_to_string("input/input.txt") {
-    //     Ok(data) => println!("Part 2: input result: {}", part2::solve(data)),
-    //     Err(e) => println!("Error reading actual input for part 2: {}", e),
-    // }
+    match fs::read_to_string("input/sample.txt") {
+        Ok(data) => println!("Part 2: sample result: {}", part2::solve(data)),
+        Err(e) => println!("Error reading sample input for part 2: {}", e),
+    }
+    match fs::read_to_string("input/input.txt") {
+        Ok(data) => println!("Part 2: input result: {}", part2::solve(data)),
+        Err(e) => println!("Error reading actual input for part 2: {}", e),
+    }
 }
