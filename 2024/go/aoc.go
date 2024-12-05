1179c1795c04dd42ae306d140f6cf4463f842150
DaveyDark <devesh97531@gmail.com>
2024-12-05 14:04:19 +0530

    2024 Day 5 Done

--- 2024/go/aoc.go
+++ 2024/go/aoc.go
@@ -9,6 +9,7 @@ import (
 	_ "github.com/daveydark/adventofcode/2024/day02"
 	_ "github.com/daveydark/adventofcode/2024/day03"
 	_ "github.com/daveydark/adventofcode/2024/day04"
+	_ "github.com/daveydark/adventofcode/2024/day05"
 	"github.com/daveydark/adventofcode/2024/internal/registry"
 )
 
